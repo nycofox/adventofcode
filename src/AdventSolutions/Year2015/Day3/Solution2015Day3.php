2eaf847179069424ff160740da8223f01158fd7b
nycofox <github@alkis.net>
2023-10-23 20:38:46 +0200

    Refactored solution for 2015 day 3

--- src/AdventSolutions/Year2015/Day3/Solution2015Day3.php
+++ src/AdventSolutions/Year2015/Day3/Solution2015Day3.php
@@ -4,105 +4,70 @@ namespace App\AdventSolutions\Year2015\Day3;
 
 class Solution2015Day3
 {
-
     private array $houses = [];
 
     public function solvePart1($input)
     {
         $directions = str_split($input[0]);
-
         $x = 0;
         $y = 0;
-
         $this->visitHouse($x, $y);
 
         foreach ($directions as $move) {
-            switch ($move) {
-                case '^':
-                    $y += 1;
-                    break;
-                case 'v':
-                    $y -= 1;
-                    break;
-                case '>':
-                    $x += 1;
-                    break;
-                case '<':
-                    $x -= 1;
-                    break;
-            }
-
+            [$dx, $dy] = $this->getDirection($move);
+            $x += $dx;
+            $y += $dy;
             $this->visitHouse($x, $y);
         }
 
         return "Santa visited " . count($this->houses) . " houses at least once";
-
     }
 
     public function solvePart2($input)
     {
         $this->houses = [];
-
         $directions = str_split($input[0]);
-
-        $realSantaX = 0;
-        $realSantaY = 0;
+        $santaX = 0;
+        $santaY = 0;
         $roboSantaX = 0;
         $roboSantaY = 0;
-        $currentSanta = 'real';
+        $this->visitHouse($santaX, $santaY);
 
-        $this->visitHouse($realSantaX, $realSantaY);
+        for ($i = 0; $i < count($directions); $i++) {
+            [$dx, $dy] = $this->getDirection($directions[$i]);
 
-        foreach ($directions as $move) {
-            // realSanta and roboSanta take turns moving
-            if ($currentSanta == 'real') {
-                switch ($move) {
-                    case '^':
-                        $realSantaY += 1;
-                        break;
-                    case 'v':
-                        $realSantaY -= 1;
-                        break;
-                    case '>':
-                        $realSantaX += 1;
-                        break;
-                    case '<':
-                        $realSantaX -= 1;
-                        break;
-                }
-                $this->visitHouse($realSantaX, $realSantaY);
-                $currentSanta = 'robo';
+            if ($i % 2 === 0) {
+                $santaX += $dx;
+                $santaY += $dy;
+                $this->visitHouse($santaX, $santaY);
             } else {
-                switch ($move) {
-                    case '^':
-                        $roboSantaY += 1;
-                        break;
-                    case 'v':
-                        $roboSantaY -= 1;
-                        break;
-                    case '>':
-                        $roboSantaX += 1;
-                        break;
-                    case '<':
-                        $roboSantaX -= 1;
-                        break;
-                }
+                $roboSantaX += $dx;
+                $roboSantaY += $dy;
                 $this->visitHouse($roboSantaX, $roboSantaY);
-                $currentSanta = 'real';
             }
-
         }
 
         return "Santa visited " . count($this->houses) . " houses at least once";
-
     }
 
     private function visitHouse($x, $y)
     {
-        if (!isset($this->houses[$x . $y])) {
-            $this->houses[$x . $y] = 1;
-        } else {
-            $this->houses[$x . $y] += 1;
+        $this->houses[$x . $y] = true;
+    }
+
+    private function getDirection($move)
+    {
+        switch ($move) {
+            case '^':
+                return [0, 1];
+            case 'v':
+                return [0, -1];
+            case '>':
+                return [1, 0];
+            case '<':
+                return [-1, 0];
+            default:
+                return [0, 0];
         }
     }
 }
